e3d3af3eb6d37957d0e649dcd357eac4c4c50eef
dsevenx <steffen.rokosch@icloud.com>
2025-06-22 17:35:57 +0200

    SpecialOffer

--- src/app/sections/SpecialOffer.tsx
+++ src/app/sections/SpecialOffer.tsx
@@ -1,8 +1,31 @@
+import Button from '@/components/Button'
+import React from 'react'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section id="SpecialOffer" className="flex justify-wrap items-center gap-10 max-container">
+
+      <div className='flex flex-1 flex-col'>
+        <h2 className="mt-10 font-palanquin text-4xl">
+          <span className="text-coral-red inline-block">Special</span> Offers
+        </h2>
+        <p className="text-lg text-slate-500 mb-10 info-text mt-10">Ensuring
+          premium comfort and style, our meticulously crafted heatwave-footwear is designed to elevate your experience.</p>
+        <p className="text-lg text-slate-500 mb-10 info-text">Our dedication todetail and excellence ensures your satisfaction.</p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label="Shop Now" iconUrl="/icons/arrow-right.svg" />
+          <Button label="Learn More" iconUrl="" 
+          backgroundColor={"bg-transparent"} textColor={"text-black"} borderColor={"border-black"} />
+        </div>
+      </div>
+      <div className="flex-1 flex justify-center items-center mt-10">
+        <img src="/images/offer.svg" alt="SpecialOffer"
+          width={773} height={687}
+          className="w-full object-contain" />
+      </div>
+    </section>
   )
 }
+
 
 export default SpecialOffer
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,18 +3,24 @@ import React from 'react'
 interface ButtonProps {
   label: string;
   iconUrl: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ label, iconUrl }) => {
+const Button: React.FC<ButtonProps> = ({ label, iconUrl, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 
-    border font-montserrat rounded-full bg-black text-white
-    hover:bg-white hover:text-black transition-colors duration-300">
+    <button className={`flex justify-center items-center gap-2 px-7 py-4 border 
+      font-montserrat rounded-full 
+      ${backgroundColor ? backgroundColor : "bg-black"}
+      ${textColor ? textColor : "text-white"}
+      ${borderColor ? borderColor : "border-transparent"}
+     hover:bg-white hover:text-black transition-colors duration-300`}>
       {label}
       {iconUrl &&
-      <img src={iconUrl} alt="Button Icon" className="ml-2 rounded-full w-5 h-5" />
-    }   
-      </button>
+        <img src={iconUrl} alt="Button Icon" className="ml-2 rounded-full w-5 h-5" />
+      }
+    </button>
   )
 }
 
